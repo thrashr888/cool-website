544bad0ef76cb2698dc088c7441c77c932520633
Paul Thrasher <pthrasher@hashicorp.com>
2018-07-25 14:42:18 -0700

    break index?

--- deploy.tf
+++ deploy.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "static_site" {
   acl    = "public-read"
 
   website {
-    index_document = "index.html"
+    index_document = "indx.html"
     error_document = "error.html"
   }
 }
