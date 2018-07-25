f4a5486dc7fc1d0b46c0187b27ec8efd615b5e34
Paul Thrasher <pthrasher@hashicorp.com>
2018-07-25 14:37:19 -0700

    clean up tf config

--- deploy.tf
+++ deploy.tf
@@ -12,8 +12,6 @@ resource "aws_s3_bucket" "static_site" {
   bucket = "cool-website"
   acl    = "public-read"
 
-  # policy = "${file("policy.json")}"
-
   website {
     index_document = "index.html"
     error_document = "error.html"
