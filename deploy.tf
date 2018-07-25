86179ee4c84218d9b618644c906befd9d36cdf9d
Paul Thrasher <pthrasher@hashicorp.com>
2018-07-25 13:49:46 -0700

    init commit

---
+++ deploy.tf
@@ -0,0 +1,35 @@
+provider "aws" {}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = "cool-website"
+  acl    = "public-read"
+
+  # policy = "${file("policy.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  etag         = "${md5(file("index.html"))}"
+  acl          = "public-read"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
+  etag         = "${md5(file("error.html"))}"
+  acl          = "public-read"
+}
+
+output "website" {
+  value = "${aws_s3_bucket.static_site.website_endpoint}"
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# My Cool Website
+
+It's really the best website. I hope it doesn't crash.
+
+## Deploy
+
+Deploys use Terraform.
+
+    > terraform plan
+    > terraform apply
