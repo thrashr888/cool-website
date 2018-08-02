f15f3f68f9cc257443bb6fb552d25fef1e8d80b0
Paul Thrasher <pthrasher@hashicorp.com>
2018-08-01 18:57:15 -0700

    bump our tf module version
    also removes the variable config in favor of env vars. will update in TFE.

--- deploy.tf
+++ deploy.tf
@@ -1,20 +1,13 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_default_region" {}
-
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_default_region}"
-}
+provider "aws" {}
 
 module "s3-website" {
   source  = "thrashr888/s3-website/aws"
-  version = "0.2.0"
+  version = "0.4.0"
 
   bucket_name = "cool-website"
 }
 
 output "website_endpoint" {
+  description = "The public url of our cool website."
   value = "${module.s3-website.website_endpoint}"
 }
